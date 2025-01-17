3ff2da153f5dcd391ec3e48f3b359b1ddd24d359
Isaziya <iisaziya16@gmail.com>
2025-01-17 21:53:55 +0700

    Add files via upload

---
+++ users_202501172027.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.users (id,nama,email,alamat) VALUES
+	 (1,'John Doe','john.doe294@example.com','Alamat Jalan 123'),
+	 (2,'Jane Smith','jane.smith808@example.com','Alamat Jalan 456'),
+	 (3,'Michael Johnson','michael.johnson179@example.com','Alamat Jalan 789'),
+	 (4,'Emily Davis','emily.davis407@example.com','Alamat Jalan 101'),
+	 (5,'David Brown','david.brown429@example.com','Alamat Jalan 202'),
+	 (6,'Sarah Wilson','sarah.wilson41@example.com','Alamat Jalan 303'),
+	 (7,'James Miller','james.miller576@example.com','Alamat Jalan 404'),
+	 (8,'Patricia Moore','patricia.moore893@example.com','Alamat Jalan 505'),
+	 (9,'Robert Taylor','robert.taylor707@example.com','Alamat Jalan 606'),
+	 (10,'Linda Anderson','linda.anderson713@example.com','Alamat Jalan 707');
